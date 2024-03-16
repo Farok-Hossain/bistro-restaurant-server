b443df4c83ac65ba4d4cc2fdbdfbe5ee4deb07cc
Farok Hossain <farokafs7@gmail.com>
2024-03-16 13:10:26 +0600

    user delete from database

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
