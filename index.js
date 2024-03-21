2c060b8f9426e9b158dcfaf0a27e25b237fbed76
Farok Hossain <farokafs7@gmail.com>
2024-03-21 17:30:50 +0600

    updated

--- index.js
+++ index.js
@@ -216,6 +216,15 @@ async function run() {
       });
     });
 
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/payments", async (req, res) => {
       const payment = req.body;
       const paymentResult = await paymentCollection.insertOne(payment);
