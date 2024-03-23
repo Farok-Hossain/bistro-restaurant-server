f5d8f9c960c387a5f7d72fcee2a111b6c6f23580
Farok Hossain <farokafs7@gmail.com>
2024-03-23 23:10:06 +0600

    updated

--- index.js
+++ index.js
@@ -244,9 +244,21 @@ async function run() {
     // stats or analytics
     app.get("/admin-stats", async (req, res) => {
       const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // this is not the best way
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce(
+        (total, payment) => total + payment.price,
+        0
+      );
 
       res.send({
         users,
+        menuItems,
+        orders,
+        revenue,
       });
     });
 
