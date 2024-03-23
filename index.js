9ccee3495d33d9a14d654e0bcfbd11f2c2c84d8b
Farok Hossain <farokafs7@gmail.com>
2024-03-23 23:22:01 +0600

    updated

--- index.js
+++ index.js
@@ -242,17 +242,33 @@ async function run() {
     });
 
     // stats or analytics
-    app.get("/admin-stats", async (req, res) => {
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
       const users = await userCollection.estimatedDocumentCount();
       const menuItems = await menuCollection.estimatedDocumentCount();
       const orders = await paymentCollection.estimatedDocumentCount();
 
       // this is not the best way
-      const payments = await paymentCollection.find().toArray();
-      const revenue = payments.reduce(
-        (total, payment) => total + payment.price,
-        0
-      );
+      // const payments = await paymentCollection.find().toArray();
+      // const revenue = payments.reduce(
+      //   (total, payment) => total + payment.price,
+      //   0
+      // );
+
+      // this is the more better way
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
 
       res.send({
         users,
