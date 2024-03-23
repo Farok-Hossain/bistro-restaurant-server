b004c56ca673eb9f75a47d74aea3a4d83c7a9bfa
Farok Hossain <farokafs7@gmail.com>
2024-03-24 00:47:57 +0600

    aggregate updated

--- index.js
+++ index.js
@@ -288,6 +288,40 @@ async function run() {
      * 3. for every item in the menu collection that you found from a payment entry (document)
      */
 
+    // using aggregate pipeline
+    app.get("/order-stats", async (req, res) => {
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $unwind: "$menuItemIds",
+          },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuItemIds",
+              foreignField: "_id",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quantity: {
+                $sum: 1,
+              },
+              revenue: {
+                $sum: "$menuItems.price",
+              },
+            },
+          },
+        ])
+        .toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
